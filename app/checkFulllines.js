8861bc8f96500df927a0e45567273306fdd7f9fc
stoeffel <schtoeffel@gmail.com>
2012-06-14 17:05:03 +0200

    no message

---
+++ app/checkFulllines.js
@@ -0,0 +1,89 @@
+function checkFullines(){
+	console.log('check')
+	// check for full lines
+			var linesCount = 0,
+				lines = {},
+				fulllines = [],
+				ys = [];
+			$.each(bricks, function(i, v) {
+				var x = v.attr('x'),
+					y = v.attr('y');
+				if (!lines[y]) lines[y] = [];
+				lines[y].push(v);
+				if (lines[y].length == 10) {
+					linesCount++;
+					fulllines = $.merge(fulllines, lines[y]);
+					ys.push(y);
+					delete lines[y];
+				}
+			});
+			if (linesCount > 0) {
+				switch (linesCount) {
+				case 1:
+					score += 10;
+					break;
+				case 2:
+					score += 25;
+					break;
+				case 3:
+					score += 40;
+					break;
+				case 4:
+					score += 60;
+					break;
+				default:
+
+					break;
+				};
+				$('#score').html(score);
+
+
+				clearInterval(gameLoop)
+				var linesToRemove = paper.set()
+				$.each(fulllines, function(i, v) {
+					linesToRemove.push(v);
+				})
+				linesToRemove.animate({
+					opacity: 0,
+					transform: 's0r360'
+				}, 1000, 'bounce', function() {
+					linesToRemove.remove();
+					bricks = [];
+					$.each(lines, function(i, v) {
+						$.merge(bricks, v);
+					});
+					ys.sort(function(a, b) {
+						return (a - b);
+					})
+					var newBricks = [];
+					var oldBricks = [];
+					$.each(ys, function(i, y) {
+						newBricks = [];
+						$.each(bricks, function(ii, brick) {
+							var yy = brick.attr('y');
+							var xx = brick.attr('x');
+							var fill = brick.attr('fill');
+							if (yy < y) {
+								oldBricks.push(brick);
+								newBricks.push(new Brick({
+									x: xx - 1,
+									y: yy - 1 + BrickSide,
+									color: fill
+								}));
+							} else {
+								newBricks.push(brick);
+							}
+						});
+						delete bricks;
+						bricks = $.merge([], newBricks);
+						delete newBricks;
+					});
+					$.each(oldBricks, function(i, o) {
+						o.remove();
+					})
+
+					gameLoop = setInterval(loop, speed)
+				});
+
+			}
+}
---
+++ previewBlock.js
@@ -0,0 +1,11 @@
+paperNextBlock.clear();
+nextBlock = new Block();
+
+nextSet = new Block(paperNextBlock, {
+	color: nextBlock.color,
+	x: BrickSide,
+	y: BrickSide,
+	angle: nextBlock.angle,
+	type: nextBlock.type
+
+});
